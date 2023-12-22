fec8f5bc856f4d98f6c98a83aaef6cbb8546bc82
Muhamad Prasetyo <muhamad.mprasetyo@gmail.com>
2023-12-22 12:31:33 +0700

    membuat Vue Router dan menambahkan code vue ke app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
 require('./bootstrap');
+
+
+// untuk bagian vue 
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+import routes from './router'
+
+Vue.component('navigation', require('./components/Navigation').default);
+const app = new Vue({
+    el: '#app',
+    router: new VueRouter(routes),
+});

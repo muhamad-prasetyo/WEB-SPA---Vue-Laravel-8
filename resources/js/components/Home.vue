d102cfaf85e5c4f770914bdf13fa9a50f8fae4fb
Muhamad Prasetyo <muhamad.mprasetyo@gmail.com>
2023-12-22 12:20:10 +0700

    menambahkan folder Components dan isi filenya

---
+++ resources/js/components/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="jumbotron">
+        <h1 class="display-4">WEBSITE SPA DEMO</h1>
+        <p class="lead">DEMO WEB VUE & LARAVEL 8 - SPA (Single Page Application)</p>
+        <hr class="my-4">
+        <router-link :to="{ name: 'about'}" class="btn btn-primary btn-lg" role="button">Selengkapnya</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
